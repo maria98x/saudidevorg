49be15b12d7f85f4cd3f5453d287df827a28fd3f
maria98x <53050052+maria98x@users.noreply.github.com>
2019-08-27 00:53:53 +0300

    Add files via upload

---
+++ week2.js
@@ -0,0 +1,18 @@
+
+function test(){
+  var Maria ="hola <Br>";
+  document.write(Maria);
+}
+test();
+
+document.write(Math.round(4.3)+"<Br>");
+
+//alert("مرحبا بكم في عالم الجافا سكربت");
+
+var todayDate = new Date();
+function days(todayDate){
+  document.write(todayDate+"<Br>"+todayDate.getDay());
+}
+days(todayDate);
+
+console.log("by this method we can print in the console");
